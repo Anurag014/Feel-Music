ac5191b4e8b1fb80f2fcf2105326335be3adfd52
Anurag Gaur <anuragaur01@gmail.com>
2024-01-02 21:46:24 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import {
+//   searchMusics,
+//   searchAlbums,
+//   searchPlaylists,
+//   getSuggestions,
+//   listMusicsFromAlbum,
+//   listMusicsFromPlaylist,
+//   searchArtists,
+//   getArtist,
+// } from 'node-youtube-music';
+
+function App() {
+
+  // const music = searchMusics("Ram Ayenge");
+  // console.log(music)
+
+  return (
+    <>
+      <h1 className='bg-red-500 p-4'>Music App</h1>
+    </>
+  )
+}
+
+export default App
